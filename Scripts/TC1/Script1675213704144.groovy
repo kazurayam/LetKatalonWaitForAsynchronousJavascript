85e4d4612775a5043c84a91533bad51ab6e3cec7
kazurayam <kazuaki.urayama@gmail.com>
2023-02-02 11:59:27 +0900

    introduce MARKER_ID

--- Scripts/TC1/Script1675213704144.groovy
+++ Scripts/TC1/Script1675213704144.groovy
@@ -14,11 +14,15 @@ TestObject makeTestObject(String xpath) {
 WebUI.openBrowser("")
 WebUI.navigateToUrl("https://katalon-demo-cura.herokuapp.com/profile.php#login")
 
+String MARKER_ID = "marker${new Date().getTime()}"
+
 String js = """
 const sleep = m => new Promise(r => setTimeout(r, m));
 (async () => {
     await sleep(3000);
-    var newElement = "<div id='marker' style='display:none'>Marker</div>";
+
+	// insert a invisible marker at the bottom of the page
+    var newElement = "<div id='${MARKER_ID}' style='display:none'>Marker</div>";
     var bodyElement = document.body;
     bodyElement.innerHTML = bodyElement.innerHTML + newElement;
 })();
@@ -27,7 +31,7 @@ const sleep = m => new Promise(r => setTimeout(r, m));
 LocalDateTime start = LocalDateTime.now()
 
 WebUI.executeJavaScript(js, null)
-WebUI.verifyElementPresent(makeTestObject("//div[@id='marker']"), 10)
+WebUI.verifyElementPresent(makeTestObject("//div[@id='${MARKER_ID}']"), 10)
 
 LocalDateTime end = LocalDateTime.now()
 WebUI.comment("${Duration.between(start,end).toMillis()} milliseconds passed")
